362b54ba0236303733b0824d9b3dc678ed161f13
Befries <adejolier@outlook.be>
2024-06-29 20:07:59 +0200

    feat: GameInfo body & GameInfoList fetching and processing of data

--- src/main/java/network/amnesia/anbd/gameinfo/GameInfo.java
+++ src/main/java/network/amnesia/anbd/gameinfo/GameInfo.java
@@ -1,6 +1,207 @@
 package network.amnesia.anbd.gameinfo;
 
-// parses Json into an object
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+
 public class GameInfo {
 
+    @JsonProperty("internalName")
+    public String getInternalName() {
+        return this.internalName;
+    }
+    public GameInfo setInternalName(String internalName) {
+        this.internalName = internalName;
+        return this;
+    }
+    private String internalName;
+
+    @JsonProperty("title")
+    public String getTitle() {
+        return this.title;
+    }
+    public GameInfo setTitle(String title) {
+        this.title = title;
+        return this;
+    }
+    private String title;
+
+    @JsonProperty("metacriticLink")
+    public String getMetacriticLink() {
+        return this.metacriticLink;
+    }
+    public GameInfo setMetacriticLink(String metacriticLink) {
+        this.metacriticLink = metacriticLink;
+        return this;
+    }
+    private String metacriticLink;
+
+    @JsonProperty("dealID")
+    public String getDealID() {
+        return this.dealID;
+    }
+    public GameInfo setDealID(String dealID) {
+        this.dealID = dealID;
+        return this;
+    }
+    private String dealID;
+
+    @JsonProperty("storeID")
+    public String getStoreID() {
+        return this.storeID;
+    }
+    public GameInfo setStoreID(String storeID) {
+        this.storeID = storeID;
+        return this;
+    }
+    private String storeID;
+
+    @JsonProperty("gameID")
+    public String getGameID() {
+        return this.gameID;
+    }
+    public GameInfo setGameID(String gameID) {
+        this.gameID = gameID;
+        return this;
+    }
+    private String gameID;
+
+    @JsonProperty("salePrice")
+    public String getSalePrice() {
+        return this.salePrice;
+    }
+    public GameInfo setSalePrice(String salePrice) {
+        this.salePrice = salePrice;
+        return this;
+    }
+    private String salePrice;
+
+    @JsonProperty("normalPrice")
+    public String getNormalPrice() {
+        return this.normalPrice;
+    }
+    public GameInfo setNormalPrice(String normalPrice) {
+        this.normalPrice = normalPrice;
+        return this;
+    }
+    private String normalPrice;
+
+    @JsonProperty("isOnSale")
+    public String getIsOnSale() {
+        return this.isOnSale;
+    }
+    public GameInfo setIsOnSale(String isOnSale) {
+        this.isOnSale = isOnSale;
+        return this;
+    }
+    private String isOnSale;
+
+    @JsonProperty("savings")
+    public String getSavings() {
+        return this.savings;
+    }
+    public GameInfo setSavings(String savings) {
+        this.savings = savings;
+        return this;
+    }
+    private String savings;
+
+    @JsonProperty("metacriticScore")
+    public String getMetacriticScore() {
+        return this.metacriticScore;
+    }
+    public GameInfo setMetacriticScore(String metacriticScore) {
+        this.metacriticScore = metacriticScore;
+        return this;
+    }
+    private String metacriticScore;
+
+    @JsonProperty("steamRatingText")
+    public String getSteamRatingText() {
+        return this.steamRatingText;
+    }
+    public GameInfo setSteamRatingText(String steamRatingText) {
+        this.steamRatingText = steamRatingText;
+        return this;
+    }
+    private String steamRatingText;
+
+    @JsonProperty("steamRatingPercent")
+    public String getSteamRatingPercent() {
+        return this.steamRatingPercent;
+    }
+    public GameInfo setSteamRatingPercent(String steamRatingPercent) {
+        this.steamRatingPercent = steamRatingPercent;
+        return this;
+    }
+    private String steamRatingPercent;
+
+    @JsonProperty("steamRatingCount")
+    public String getSteamRatingCount() {
+        return this.steamRatingCount;
+    }
+    public GameInfo setSteamRatingCount(String steamRatingCount) {
+        this.steamRatingCount = steamRatingCount;
+        return this;
+    }
+    private String steamRatingCount;
+
+    @JsonProperty("steamAppID")
+    public String getSteamAppID() {
+        return this.steamAppID;
+    }
+    public GameInfo setSteamAppID(String steamAppID) {
+        this.steamAppID = steamAppID;
+        return this;
+    }
+    private String steamAppID;
+
+    @JsonProperty("releaseDate")
+    public int getReleaseDate() {
+        return this.releaseDate;
+    }
+    public GameInfo setReleaseDate(int releaseDate) {
+        this.releaseDate = releaseDate;
+        return this;
+    }
+    private int releaseDate;
+
+    @JsonProperty("lastChange")
+    public int getLastChange() {
+        return this.lastChange;
+    }
+    public GameInfo setLastChange(int lastChange) {
+        this.lastChange = lastChange;
+        return this;
+    }
+    private int lastChange;
+
+    @JsonProperty("dealRating")
+    public String getDealRating() {
+        return this.dealRating;
+    }
+    public GameInfo setDealRating(String dealRating) {
+        this.dealRating = dealRating;
+        return this;
+    }
+    private String dealRating;
+
+    @JsonProperty("thumb")
+    public String getThumb() {
+        return this.thumb;
+    }
+    public GameInfo setThumb(String thumb) {
+        this.thumb = thumb;
+        return this;
+    }
+    private String thumb;
+
+
+    // too check if two GameInfos are about the same game, check if they have the same gameId
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof GameInfo) return Objects.equals(gameID, ((GameInfo) obj).gameID);
+        return super.equals(obj);
+    }
 }
--- src/main/java/network/amnesia/anbd/gameinfo/GameInfoList.java
+++ src/main/java/network/amnesia/anbd/gameinfo/GameInfoList.java
@@ -1,7 +1,132 @@
 package network.amnesia.anbd.gameinfo;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.ImmutableList;
+import okhttp3.*;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Objects;
+import java.util.regex.MatchResult;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+
 // List of the games given for a specific search as well as the information of the which one is currently displayed
 public class GameInfoList {
+
+    /**
+     * regex expression to split the json objects encapsulated in {@code {...}}. This means that the objects cannot
+     * contain other sub objects encapsulated in {@code {...}}, Luckily it is not a problem as the kind of json returned
+     * by the CheapShark API follow this rule.
+     */
+    private static final String regex = "(?s)\\{[^\\{\\}]*\\}";
+    private static final String urlBase = "https://www.cheapshark.com/api/1.0/deals?";
+
+    public static int DEFAULT_PAGE_SIZE = 10;
+    public static SortingCriteria DEFAULT_SORTING_CRITERIA = SortingCriteria.REVIEWS;
+
+
+    // search fields (in case we want to display them later)
+    private final String searchTitle;
+    private final int quantity;
+    private final SortingCriteria sortingCriteria;
+
+    // storage of gameInfos
+    private final List<GameInfo> gameInfos;
+
+    // TODO: selector to know which game info is displayed
+    //       and all the query needs of the GameInfoManager
+
+
+    // there's probably a less stupid way to do that with JSONObjects but whatever if it works
+    private Stream<String> splitJsonObjects(String jsonString) {
+        return Pattern
+                .compile(regex, Pattern.MULTILINE)
+                .matcher(jsonString)
+                .results()
+                .map(MatchResult::group);
+    }
+
+    // uses default values
+    public GameInfoList(String searchTitle) {
+        this(searchTitle, DEFAULT_PAGE_SIZE, DEFAULT_SORTING_CRITERIA);
+    }
+
+    public GameInfoList(String searchTitle, int quantity, SortingCriteria sortingCriteria) {
+        this.searchTitle = searchTitle;
+        this.quantity = quantity;
+        this.sortingCriteria = sortingCriteria;
+        gameInfos = loadGameInfos();
+    }
+
+
+    private List<GameInfo> loadGameInfos() {
+        ObjectMapper jsonConverter = new ObjectMapper();
+        return splitJsonObjects(fetchRemote(getLinkQuery())).map((elem) -> {
+            try {
+                return jsonConverter.readValue(elem, GameInfo.class);
+            } catch (JsonProcessingException e) {
+                System.out.println("Invalid Json object");
+                return null; // if the object is invalid, remove it
+            }
+        }).filter(Objects::nonNull).toList();
+    }
+
+
+    private String getLinkQuery() {
+        return urlBase +
+                "title=" + searchTitle +
+                "&sortBy=" + sortingCriteria.getValue() +
+                "&pageSize=" + quantity;
+    }
+
+
+    private String fetchRemote(String link) {
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        RequestBody body = RequestBody.create(MediaType.parse("text/plain"), "");
+        Request request = new Request.Builder()
+                .url(link)
+                .method("GET", body)
+                .build();
+
+        Response response = null;
+        try {
+            response = client.newCall(request).execute();
+        } catch (IOException e) {
+            // TODO: setup ephemeral message if failure
+            // temporary solution
+            System.out.println("problem while requesting data to the CheapShark API");
+        }
+
+        // debug:
+        System.out.println(response.message());
+        return response.message();
+    }
+
+
+    public enum SortingCriteria {
+
+        DEAL_RATING("DealRating"),
+        TITLE("Title"),
+        SAVINGS("Savings"),
+        PRICE("Price"),
+        METACRITICS("Metacritic"),
+        REVIEWS("Reviews"),
+        RELEASE("Release"),
+        STORE("Store"),
+        RECENT("Recent");
+
+        private final String value;
+        SortingCriteria(String value) {
+            this.value = value;
+        }
+
+        public String getValue() {
+            return value;
+        }
+    }
 
 }
